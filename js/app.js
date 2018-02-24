b78bcdbc3f7d5f1908b6655367f10d1261790a6b
Vittorio Minacori <vittoriominacori@gmail.com>
2018-02-24 11:19:48 +0100

    making token sale a little more static

--- js/app.js
+++ js/app.js
@@ -283,6 +283,122 @@ var App = {
                 }
             }
         });
+    },
+
+    shakaTokenSale: async function (crowdsaleAddress) {
+        App.init();
+
+        Vue.use(VeeValidate);
+        await App.initCrowdsale();
+        await App.initToken();
+
+        new Vue({
+            el: '#crowdsale-details',
+            data: {
+                qrcodeVisible: false,
+                copied: false,
+                usAgreement: false,
+                countryAgreement: false,
+                globalAgreement: false,
+                funds: 1,
+                txHash: '',
+                makingTransaction: false,
+                crowdsale: {
+                    cap: 0,
+                    goal: 0,
+                    weiRaised: 0,
+                    soldTokens: 0,
+                    rate: 0,
+                    startTime: 0,
+                    endTime: 0,
+                    hasStarted: false,
+                    hasEnded: true,
+                    paused: true,
+                    projectInfo: {}
+                },
+                token: {
+                    crowdsaleSupply: 0
+                }
+            },
+            created: async function () {
+                const crowdsale = await App.contracts.FriendsFingersCrowdsale.at(crowdsaleAddress);
+                const token = await App.contracts.FriendsFingersToken.at(await crowdsale.token());
+
+                this.token.address = token.address;
+                this.token.name = "Shaka";
+                this.token.symbol = "HAK";
+                this.token.decimals = 18;
+
+                this.crowdsale.address = crowdsale.address;
+                this.crowdsale.cap = 2000;
+                this.crowdsale.goal = 0;
+                this.crowdsale.weiRaised = App.web3.fromWei(await crowdsale.weiRaised()).valueOf();
+                this.crowdsale.rate = 4800;
+                this.crowdsale.soldTokens = (App.web3.toWei(this.crowdsale.weiRaised) * this.crowdsale.rate) / Math.pow(10, this.token.decimals);
+                this.crowdsale.startTime = 1519470000 * 1000;
+                this.crowdsale.startTimeFormatted = new Date(this.crowdsale.startTime).toLocaleString();
+                this.crowdsale.endTime = 1521889200 * 1000;
+                this.crowdsale.endTimeFormatted = new Date(this.crowdsale.endTime).toLocaleString();
+                this.crowdsale.projectInfo = JSON.parse(await crowdsale.crowdsaleInfo());
+                this.crowdsale.hasStarted = Date.now() > this.crowdsale.startTime;
+                this.crowdsale.hasEnded = await crowdsale.hasEnded();
+                this.crowdsale.paused = await crowdsale.paused();
+
+                this.token.crowdsaleSupply = this.crowdsale.cap * this.crowdsale.rate;
+
+                $('.crowdsale-box').toggleClass('d-none');
+            },
+            methods: {
+                viewQRCode: function () {
+                    if (!this.qrcodeVisible) {
+                        var qr = document.getElementById("crowdsale-qrcode");
+                        new QRCode(qr, this.crowdsale.address);
+                        this.qrcodeVisible = true;
+                    }
+                },
+                copyAddress: function () {
+                    if (this.wallet !== '' && !this.address_error) {
+                        document.querySelector("#crowdsale-address").select();
+                        try {
+                            this.copied = document.execCommand("copy");
+                        } catch (err) {
+                            this.copied = false;
+                        }
+                    }
+                },
+                fundCrowdsale: async function () {
+                    if (!App.metamask.installed) {
+                        alert("To use the invest button please install MetaMask extension for Chrome or Firefox!");
+                        return;
+                    } else {
+                        if (App.metamask.netId !== networkId) {
+                            alert("Your MetaMask extension in on the wrong network. Please switch on " + networkName + " and try again!");
+                            return;
+                        }
+                    }
+
+                    this.$validator.validateAll().then(async (result) => {
+
+                        if (result) {
+                            const crowdsale = await App.contracts.FriendsFingersCrowdsale.at(crowdsaleAddress);
+                            const value = App.web3.toWei(this.funds);
+                            try {
+                                this.txHash = '';
+                                this.makingTransaction = true;
+                                const log = await crowdsale.send(value);
+                                this.txHash = log.tx;
+                                console.log(log);
+                            } catch (e) {
+                                alert("Some error occurred. Maybe you rejected the transaction or you have MetaMask locked!");
+                                this.makingTransaction = false;
+                            }
+                        } else {
+                            console.log("some errors");
+                        }
+                    });
+                }
+            }
+        });
     }
 };
 
@@ -295,7 +411,7 @@ var App = {
             break;
 
         case "shaka-token-sale":
-            App.viewCrowdsale(FriendsFingersTokenSaleAddress);
+            App.shakaTokenSale(FriendsFingersTokenSaleAddress);
             break;
 
         case "crowdsale":
